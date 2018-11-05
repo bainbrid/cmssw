c8bb6473bd9f905e882cd979fb8a5eabf8a4164d
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-11-05 15:33:01 +0000

    tidy up

--- RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronProducer.cc
+++ RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronProducer.cc
@@ -1,12 +1,11 @@
 #include "RecoEgamma/EgammaElectronProducers/plugins/LowPtGsfElectronProducer.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
-#include "DataFormats/EgammaReco/interface/ElectronSeed.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronCore.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronCoreFwd.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
-#include "DataFormats/TrackCandidate/interface/TrackCandidateCollection.h"
-#include "DataFormats/TrackingRecHit/interface/TrackingRecHitFwd.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrackFwd.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -26,23 +25,22 @@ LowPtGsfElectronProducer::~LowPtGsfElectronProducer()
 
 void LowPtGsfElectronProducer::produce( edm::Event& event, const edm::EventSetup& setup )
 {
-  if (true) { // default Egamma logic
-    beginEvent(event,setup);
-    algo_->completeElectrons(globalCache());
-    fillEvent(event);
-    endEvent();
-  } else { // simple logic to promote Core to final electron
-    auto electrons = std::make_unique<GsfElectronCollection>();
-    edm::Handle<reco::GsfElectronCoreCollection> coreElectrons;
-    event.getByToken(inputCfg_.gsfElectronCores,coreElectrons);
-    for ( unsigned int ii=0; ii < coreElectrons->size(); ++ii ) {
-      const GsfElectronCoreRef ref = edm::Ref<GsfElectronCoreCollection>(coreElectrons,ii);
-      GsfElectron* ele = new GsfElectron(ref);
-      const GsfTrackRef& gsf = ref->gsfTrack();
-      ele->setP4(GsfElectron::P4_FROM_SUPER_CLUSTER,Candidate::LorentzVector(gsf->px(),gsf->py(),gsf->pz(),0.511E-3),0,true) ;
-      LogTrace("GsfElectronAlgo")<<"Constructed new electron with energy  "<< ele->p4().e() ;
-      electrons->push_back(*ele) ;
-    }
-    event.put(std::move(electrons));
+  auto electrons = std::make_unique<GsfElectronCollection>();
+  edm::Handle<reco::GsfElectronCoreCollection> coreElectrons;
+  event.getByToken(inputCfg_.gsfElectronCores,coreElectrons);
+  for ( unsigned int ii=0; ii < coreElectrons->size(); ++ii ) {
+    const GsfElectronCoreRef ref = edm::Ref<GsfElectronCoreCollection>(coreElectrons,ii);
+    GsfElectron* ele = new GsfElectron(ref);
+    const GsfTrackRef& gsf = ref->gsfTrack();
+    ele->setP4(GsfElectron::P4_FROM_SUPER_CLUSTER,
+	       Candidate::LorentzVector(gsf->px(),gsf->py(),gsf->pz(),0.511E-3),
+	       0,
+	       true);
+    LogTrace("GsfElectronAlgo") 
+      << "[LowPtGsfElectronProducer::produce]"
+      << " Constructed new electron with energy " 
+      << ele->p4().e();
+    electrons->push_back(*ele) ;
   }
+  event.put(std::move(electrons));
 }
