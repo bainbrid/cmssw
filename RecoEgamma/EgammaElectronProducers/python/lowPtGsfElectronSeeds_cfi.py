d840c0319e920c9de3356b95b1f68fb62d3a455e
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-11-06 16:44:55 +0000

    added track extrap, gsf tracking, BDT via HeavyObjectCache

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSeeds_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSeeds_cfi.py
@@ -4,4 +4,9 @@ lowPtGsfElectronSeeds = cms.EDProducer(
     "LowPtGsfElectronSeedProducer",
     tracks = cms.InputTag("generalTracks"),
     clusters = cms.InputTag("particleFlowClusterECAL"),
+    Fitter = cms.string('GsfTrajectoryFitter_forPreId'),
+    Smoother = cms.string('GsfTrajectorySmoother_forPreId'),
+    TTRHBuilder = cms.string('WithAngleAndTemplate'),
+    Weights = cms.string('RecoEgamma/EgammaElectronProducers/data/BDT.xml'),
+    PassThrough = cms.bool(False),
     )
