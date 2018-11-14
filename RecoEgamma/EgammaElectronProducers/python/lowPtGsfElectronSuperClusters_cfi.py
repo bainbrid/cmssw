9002447e2e5bad840d0f5fb17e69618b2dfd4871
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-11-14 11:02:44 +0000

    updated collection name

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSuperClusters_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSuperClusters_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 lowPtGsfElectronSuperClusters = cms.EDProducer(
     "LowPtGsfElectronSCProducer",
-    gsfPfRecTracks = cms.InputTag("pfTrackElec"),
+    gsfPfRecTracks = cms.InputTag("lowPtGsfElePfGsfTracks"),
     ecalClusters = cms.InputTag("particleFlowClusterECAL"),
     hcalClusters = cms.InputTag("particleFlowClusterHCAL"),
 )
