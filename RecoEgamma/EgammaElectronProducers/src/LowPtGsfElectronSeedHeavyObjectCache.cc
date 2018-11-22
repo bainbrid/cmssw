0184f11f1b6d89ba8fb2086a064e3419973c2812
Robert Bainbridge <bainbrid@users.noreply.github.com>
2018-11-22 16:14:14 +0000

    use GBRForestTools, remove var binding, add eval methods

--- RecoEgamma/EgammaElectronProducers/src/LowPtGsfElectronSeedHeavyObjectCache.cc
+++ RecoEgamma/EgammaElectronProducers/src/LowPtGsfElectronSeedHeavyObjectCache.cc
@@ -1,33 +1,79 @@
+#include "DataFormats/ParticleFlowReco/interface/PreId.h"
 #include "RecoEgamma/EgammaElectronProducers/interface/LowPtGsfElectronSeedHeavyObjectCache.h"
+#include "RecoEgamma/EgammaTools/interface/GBRForestTools.h"
 #include "TMVA/MethodBDT.h"
 #include "TMVA/Reader.h"
 #include <string>
 
 namespace lowptgsfeleseed {
+
+  ////////////////////////////////////////////////////////////////////////////////
+  //
   HeavyObjectCache::HeavyObjectCache( const edm::ParameterSet& conf ) {
-    
-    edm::FileInPath weights( conf.getParameter<edm::FileInPath>("Weights") );
-    
-    TMVA::Reader reader("!Color:Silent");
-    reader.AddVariable("trk_pt",&trk_pt);
-    reader.AddVariable("trk_eta",&trk_eta);
-    reader.AddVariable("trk_phi",&trk_phi);
-    reader.AddVariable("trk_p",&trk_p);
-    reader.AddVariable("trk_charge",&trk_charge);
-    reader.AddVariable("trk_nhits",&trk_nhits);
-    reader.AddVariable("trk_high_purity",&trk_high_purity);
-    reader.AddVariable("trk_inp",&trk_inp);
-    reader.AddVariable("trk_outp",&trk_outp);
-    reader.AddVariable("trk_chi2red",&trk_chi2red);
-    reader.AddVariable("preid_trk_ecal_Deta",&preid_trk_ecal_Deta);
-    reader.AddVariable("preid_trk_ecal_Dphi",&preid_trk_ecal_Dphi);
-    reader.AddVariable("preid_e_over_p",&preid_e_over_p);
-    reader.AddVariable("preid_gsf_dpt",&preid_gsf_dpt);
-    reader.AddVariable("preid_trk_gsf_chiratio",&preid_trk_gsf_chiratio);
-    reader.AddVariable("preid_gsf_chi2red",&preid_gsf_chi2red);
-    reader.BookMVA("BDT", weights.fullPath().c_str());
-    
-    gbr.reset( new GBRForest(dynamic_cast<TMVA::MethodBDT*>(reader.FindMVA("BDT"))) );
-    
+    edm::FileInPath unbiased( conf.getParameter<edm::FileInPath>("UnbiasedWeights") );
+    edm::FileInPath ptbiased( conf.getParameter<edm::FileInPath>("PtBiasedWeights") );
+    unbiased_ = GBRForestTools::createGBRForest(unbiased);
+    ptbiased_ = GBRForestTools::createGBRForest(ptbiased);
+    unbiasedThreshold_ = conf.getParameter<double>("UnbiasedThreshold");
+    ptbiasedThreshold_ = conf.getParameter<double>("PtBiasedThreshold");
   }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  bool HeavyObjectCache::evalUnbiased( reco::PreId& ecal, reco::PreId& hcal ) const {
+
+    float features[16] = { 
+      (float)ecal.trackRef()->pt(),
+      (float)ecal.trackRef()->eta(),
+      (float)ecal.trackRef()->phi(),
+      (float)ecal.trackRef()->p(),
+      (float)ecal.trackRef()->charge(),
+      (float)ecal.trackRef()->found(),
+      (float)ecal.trackRef()->quality(reco::TrackBase::qualityByName("highPurity")),
+      (float)sqrt( ecal.trackRef()->innerMomentum().Mag2() ),
+      (float)sqrt( ecal.trackRef()->outerMomentum().Mag2() ),
+      (float)ecal.trackRef()->normalizedChi2(),
+      (float)ecal.geomMatching()[0],
+      (float)ecal.geomMatching()[1],
+      (float)ecal.eopMatch(),
+      (float)ecal.dpt(),
+      (float)ecal.chi2Ratio(),
+      (float)ecal.gsfChi2()
+    };
+    float output = unbiased_->GetClassifier(features);
+    bool pass = output > unbiasedThreshold_;
+    ecal.setMVA( pass, output );
+    return pass;
+
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  bool HeavyObjectCache::evalPtBiased( reco::PreId& ecal, reco::PreId& hcal ) const {
+
+    float features[16] = { 
+      (float)ecal.trackRef()->pt(),
+      (float)ecal.trackRef()->eta(),
+      (float)ecal.trackRef()->phi(),
+      (float)ecal.trackRef()->p(),
+      (float)ecal.trackRef()->charge(),
+      (float)ecal.trackRef()->found(),
+      (float)ecal.trackRef()->quality(reco::TrackBase::qualityByName("highPurity")),
+      (float)sqrt( ecal.trackRef()->innerMomentum().Mag2() ),
+      (float)sqrt( ecal.trackRef()->outerMomentum().Mag2() ),
+      (float)ecal.trackRef()->normalizedChi2(),
+      (float)ecal.geomMatching()[0],
+      (float)ecal.geomMatching()[1],
+      (float)ecal.eopMatch(),
+      (float)ecal.dpt(),
+      (float)ecal.chi2Ratio(),
+      (float)ecal.gsfChi2()
+    };
+    float output = ptbiased_->GetClassifier(features);
+    bool pass = output > ptbiasedThreshold_;
+    ecal.setMVA( pass, output );
+    return pass;
+
+  }
+
 }
