355e62b7237b9e6d054c7d0fc64e1c01cc062ad4
bainbrid <bainbrid@users.noreply.github.com>
2020-08-13 11:59:14 +0200

    missing import

--- PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
-
+from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cfi import *
 from PhysicsTools.PatAlgos.slimming.slimmedLowPtElectrons_cfi import *
-
 from Configuration.Eras.Modifier_bParking_cff import bParking
 bParking.toModify(lowPtGsfElectronID, 
-                  electrons='slimmedLowPtElectrons'
+                  electrons='slimmedLowPtElectrons',
                   rho='fixedGridRhoFastjetAll')
