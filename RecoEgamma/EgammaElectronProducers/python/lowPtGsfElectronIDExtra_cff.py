c5f798509d59f8ec3f5b74a37a272ce3d5ee2621
bainbrid <bainbrid@users.noreply.github.com>
2020-09-15 14:54:27 +0200

    Switch to 2020Sept15 model (depth=13, ntrees=1000) and PAT inputs

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronIDExtra_cff.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronIDExtra_cff.py
@@ -3,9 +3,11 @@ import FWCore.ParameterSet.Config as cms
 from RecoEgamma.EgammaElectronProducers.defaultLowPtGsfElectronIDExtra_cfi import defaultLowPtGsfElectronIDExtra
 
 lowPtGsfElectronIDExtra = defaultLowPtGsfElectronIDExtra.clone(
+    electrons = 'slimmedLowPtElectrons',
+    rho = 'fixedGridRhoFastjetAll',
     ModelNames = cms.vstring(['']),
     ModelWeights = cms.vstring([
-            'RecoEgamma/ElectronIdentification/data/LowPtElectrons/RunII_Autumn18_LowPtElectrons_mva_id_2020Jul26_depth13_ntrees700_finalR.xml.gz',
+            'RecoEgamma/ElectronIdentification/data/LowPtElectrons/LowPtElectrons_ID_2020Sept15.root',
             ]),
-    ModelThresholds = cms.vdouble([-10.])
+    ModelThresholds = cms.vdouble([-99.])
     )
