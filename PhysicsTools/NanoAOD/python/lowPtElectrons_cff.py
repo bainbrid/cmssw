e4fde4e78e1560cc2da8dc6aa03b0050e1495cfa
bainbrid <bainbrid@users.noreply.github.com>
2021-05-06 17:21:03 +0200

    add regression, MC tables, and 2nd version of mini rel iso

--- PhysicsTools/NanoAOD/python/lowPtElectrons_cff.py
+++ PhysicsTools/NanoAOD/python/lowPtElectrons_cff.py
@@ -3,12 +3,82 @@ from PhysicsTools.NanoAOD.nano_eras_cff import *
 from PhysicsTools.NanoAOD.common_cff import *
 
 ################################################################################
-# slimmedLowPtElectrons_WithUserData
+# Modules
 ################################################################################
+
+from RecoEgamma.EgammaTools.regressionModifier_cfi import regressionModifier106XUL
+
+_lowPtRegressionModifier = regressionModifier106XUL.clone(
+    modifierName = 'EGRegressionModifierV3',
+    rhoTag = 'fixedGridRhoFastjetAll',
+    eleRegs = dict(
+        ecalOnlyMean = dict(
+            lowEtHighEtBoundary = 20.,
+            ebLowEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_mean",
+            ebHighEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_mean",
+            eeLowEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_mean",
+            eeHighEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_mean",
+            ),
+        ecalOnlySigma = dict(
+            lowEtHighEtBoundary = 20.,
+            ebLowEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_sigma",
+            ebHighEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_sigma",
+            eeLowEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_sigma",
+            eeHighEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_sigma",
+            ),
+        epComb = dict(
+            ecalTrkRegressionConfig = dict(
+                lowEtHighEtBoundary = 20.,
+                ebLowEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_mean",
+                ebHighEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_mean",
+                eeLowEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_mean",
+                eeHighEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_mean",
+                ),
+            ecalTrkRegressionUncertConfig = dict(
+                lowEtHighEtBoundary = 20.,
+                ebLowEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_sigma",
+                ebHighEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_sigma",
+                eeLowEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_sigma",
+                eeHighEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_sigma",
+                ),
+        )
+    ),
+)
+
+import PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi
+
+updatedLowPtElectrons = cms.EDProducer(
+    "PATElectronUpdater",
+    src = cms.InputTag("slimmedLowPtElectrons"),
+    vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    computeMiniIso = cms.bool(True),
+    fixDxySign = cms.bool(True),
+    pfCandsForMiniIso = cms.InputTag("packedPFCandidates"),
+    miniIsoParamsB = PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi.patElectrons.miniIsoParamsB,
+    miniIsoParamsE = PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi.patElectrons.miniIsoParamsE,
+    applyEnergyRegression = cms.bool(True),
+    regressionConfig = _lowPtRegressionModifier,
+)
+
+#lowPtPATElectronID = cms.EDProducer(
+#    "LowPtGsfElectronIDProducer",
+#    electrons = cms.InputTag("updatedLowPtElectrons"),
+#    unbiased = cms.InputTag(""),
+#    rho = cms.InputTag("fixedGridRhoFastjetAll"),
+#    ModelNames = cms.vstring(['']),
+#    ModelWeights = cms.vstring([
+#        'RecoEgamma/ElectronIdentification/data/LowPtElectrons/LowPtElectrons_ID_2020Nov28.root',
+#    ]),
+#    ModelThresholds = cms.vdouble([-99.]),
+#    PassThrough = cms.bool(False),
+#    MinPtThreshold = cms.double(0.5),
+#    MaxPtThreshold = cms.double(15.),
+#    Version = cms.string('V1'),
+#)
 
 isoForLowPtEle = cms.EDProducer(
     "EleIsoValueMapProducer",
-    src = cms.InputTag("slimmedLowPtElectrons"),
+    src = cms.InputTag("updatedLowPtElectrons"),
     relative = cms.bool(False),
     rho_MiniIso = cms.InputTag("fixedGridRhoFastjetAll"),
     rho_PFIso = cms.InputTag("fixedGridRhoFastjetAll"),
@@ -16,10 +86,11 @@ isoForLowPtEle = cms.EDProducer(
     EAFile_PFIso = cms.FileInPath("RecoEgamma/ElectronIdentification/data/Fall17/effAreaElectrons_cone03_pfNeuHadronsAndPhotons_94X.txt"),
 )
 
-slimmedLowPtElectronsWithUserData = cms.EDProducer(
+updatedLowPtElectronsWithUserData = cms.EDProducer(
     "PATElectronUserDataEmbedder",
-    src = cms.InputTag("slimmedLowPtElectrons"),
+    src = cms.InputTag("updatedLowPtElectrons"),
     userFloats = cms.PSet(
+        #ID = cms.InputTag("lowPtPATElectronID"),
         miniIsoChg = cms.InputTag("isoForLowPtEle:miniIsoChg"),
         miniIsoAll = cms.InputTag("isoForLowPtEle:miniIsoAll"),
     ),
@@ -28,13 +99,9 @@ slimmedLowPtElectronsWithUserData = cms.EDProducer(
     userCands = cms.PSet(),
 )
 
-################################################################################
-# finalElectrons
-################################################################################
-
 finalLowPtElectrons = cms.EDFilter(
     "PATElectronRefSelector",
-    src = cms.InputTag("slimmedLowPtElectronsWithUserData"),
+    src = cms.InputTag("updatedLowPtElectronsWithUserData"),
     cut = cms.string("pt > 1. && electronID('ID') > -0.25"),
 )
 
@@ -58,8 +125,16 @@ lowPtElectronTable = cms.EDProducer(
         unbiased = Var("electronID('unbiased')",float,doc="ElectronSeed, pT- and dxy- agnostic BDT (raw) score"),
         ptbiased = Var("electronID('ptbiased')",float,doc="ElectronSeed, pT- and dxy- dependent BDT (raw) score"),
         # Isolation
-        miniPFRelIso_chg = Var("userFloat('miniIsoChg')/pt",float,doc="mini PF rel. iso., charged component"),
-        miniPFRelIso_all = Var("userFloat('miniIsoAll')/pt",float,doc="mini PF rel. iso., total (with scaled rho*EA PU corrections)"),
+        miniRelIso_chg = Var("miniPFIsolation().chargedHadronIso()/pt",float,
+                             doc="mini PF rel. iso. (charged) from miniPFIsolation()"),
+        miniRelIso_all = Var("(miniPFIsolation().chargedHadronIso()"+\
+                             "+miniPFIsolation().neutralHadronIso()"+\
+                             "+miniPFIsolation().photonIso())/pt",float,
+                             doc="mini PF rel. iso. (total) from miniPFIsolation()"),
+        miniPFRelIso_chg = Var("userFloat('miniIsoChg')/pt",float,
+                               doc="mini PF rel. iso., charged component"),
+        miniPFRelIso_all = Var("userFloat('miniIsoAll')/pt",float,
+                               doc="mini PF rel. iso., total (with scaled rho*EA PU corrections)"),
         # Misc
         convVeto = Var("passConversionVeto()",bool,doc="pass conversion veto"),
         lostHits = Var("gsfTrack.hitPattern.numberOfLostHits('MISSING_INNER_HITS')","uint8",doc="number of missing inner hits"),
@@ -85,8 +160,41 @@ lowPtElectronTable = cms.EDProducer(
 )
 
 ################################################################################
-# electronTable END
+# electronTable (MC)
 ################################################################################
 
-lowPtElectronSequence = cms.Sequence(isoForLowPtEle + slimmedLowPtElectronsWithUserData + finalLowPtElectrons)
+lowPtElectronsMCMatchForTable = cms.EDProducer(
+    "MCMatcher",                                     # cut on deltaR, deltaPt/Pt; pick best by deltaR
+    src         = lowPtElectronTable.src,            # final reco collection
+    matched     = cms.InputTag("finalGenParticles"), # final mc-truth particle collection
+    mcPdgId     = cms.vint32(11),           # one or more PDG ID (11 = ele); absolute values (see below)
+    checkCharge = cms.bool(False),          # True = require RECO and MC objects to have the same charge
+    mcStatus    = cms.vint32(1),            # PYTHIA status code (1 = stable, 2 = shower, 3 = hard scattering)
+    maxDeltaR   = cms.double(0.3),          # Minimum deltaR for the match
+    maxDPtRel   = cms.double(0.5),          # Minimum deltaPt/Pt for the match
+    resolveAmbiguities    = cms.bool(True), # Forbid two RECO objects to match to the same GEN object
+    resolveByMatchQuality = cms.bool(True), # False = just match input in order; True = pick lowest deltaR pair first
+)
+
+lowPtElectronMCTable = cms.EDProducer(
+    "CandMCMatchTableProducer",
+    src     = lowPtElectronTable.src,
+    mcMap   = cms.InputTag("lowPtElectronsMCMatchForTable"),
+    objName = lowPtElectronTable.name,
+    objType = lowPtElectronTable.name,
+    branchName = cms.string("genPart"),
+    docString = cms.string("MC matching to status==1 electrons"),
+)
+
+################################################################################
+# Sequences
+################################################################################
+
+lowPtElectronSequence = cms.Sequence(updatedLowPtElectrons
+                                     #+lowPtPATElectronID
+                                     +isoForLowPtEle
+                                     +updatedLowPtElectronsWithUserData
+                                     +finalLowPtElectrons)
 lowPtElectronTables = cms.Sequence(lowPtElectronTable)
+lowPtElectronMC = cms.Sequence(lowPtElectronsMCMatchForTable
+                               +lowPtElectronMCTable)
