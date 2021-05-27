92ac624976c5abe253e00a6de606e26e80a4fc21
bainbrid <bainbrid@users.noreply.github.com>
2021-05-27 10:59:20 +0200

    add generic low-pt ele modifier; embed conversions flags and vars

---
+++ RecoEgamma/EgammaTools/plugins/LowPtElectronsModifier.cc
@@ -0,0 +1,73 @@
+#include "CommonTools/CandAlgos/interface/ModifyObjectValueBase.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "RecoEgamma/EgammaTools/interface/LowPtConversion.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//
+class LowPtElectronModifier : public ModifyObjectValueBase
+{
+
+public:
+
+  LowPtElectronModifier(const edm::ParameterSet& conf, edm::ConsumesCollector&);
+  ~LowPtElectronModifier() override {};
+
+  void setEvent(const edm::Event&) final;
+  void setEventContent(const edm::EventSetup&) final;
+
+  void modifyObject(pat::Electron& ele) const final;
+
+private:
+
+  const edm::EDGetTokenT<edm::View<reco::Conversion> > convT_;
+  edm::Handle<edm::View<reco::Conversion> > convH_;
+  const edm::EDGetTokenT<reco::BeamSpot> bsT_;
+  edm::Handle<reco::BeamSpot> bsH_;
+  bool extra_;
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+//
+LowPtElectronModifier::LowPtElectronModifier(const edm::ParameterSet& conf, edm::ConsumesCollector& cc) : 
+  ModifyObjectValueBase(conf),
+  convT_(cc.consumes<edm::View<reco::Conversion> >(conf.getParameter<edm::InputTag>("conversions"))),
+  convH_(),
+  bsT_(cc.consumes<reco::BeamSpot>(conf.getParameter<edm::InputTag>("beamSpot"))),
+  bsH_(),
+  extra_(conf.getParameter<bool>("addExtraUserVars"))
+{;}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+void LowPtElectronModifier::setEvent(const edm::Event& iEvent) {
+  iEvent.getByToken(convT_, convH_);
+  if (!convH_.isValid()) {
+    throw cms::Exception("InvalidHandle", "Problem accessing Conversions collection");
+  }
+  iEvent.getByToken(bsT_, bsH_);
+  if (!convH_.isValid()) {
+    throw cms::Exception("InvalidHandle", "Problem accessing BeamSpot collection");
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+void LowPtElectronModifier::setEventContent(const edm::EventSetup& iSetup) {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+void LowPtElectronModifier::modifyObject(pat::Electron& ele) const {
+  LowPtConversion conv;
+  LowPtConversion::match(bsH_,convH_,ele,conv);
+  conv.addUserVars(ele);
+  if (extra_) { conv.addExtraUserVars(ele); }
+}
+ 
+////////////////////////////////////////////////////////////////////////////////
+//
+DEFINE_EDM_PLUGIN(ModifyObjectValueFactory,
+                  LowPtElectronModifier,
+                  "LowPtElectronModifier");
---
+++ RecoEgamma/EgammaTools/python/lowPtElectronModifier_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+lowPtElectronModifier = cms.PSet(
+    modifierName = cms.string('LowPtElectronModifier'),
+    beamSpot = cms.InputTag('offlineBeamSpot'),
+    conversions = cms.InputTag('gsfTracksOpenConversions:gsfTracksOpenConversions'),
+    addExtraUserVars = cms.bool(True),
+)
